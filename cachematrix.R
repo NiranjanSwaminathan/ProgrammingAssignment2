0a76aa62f481ba226646c31801f56f59d724c78d
NiranjanSwaminathan <niranjan.swaminathan@gmail.com>
2014-10-18 13:21:16 +0530

    Function Script
    Note: Script only defines function and they have to be called seperately
    later.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The script hold two functions , the first returns a list of functions,
+## a  getdata which returns the matrix passed, get inverse that returns inverse set
+## and set inverse used to set inverse variable at the first run.
+## The second function checks for the inverse , if it is null computes stores and 
+##displays inverse, else reads from cache and displays the same.
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+inverse<-NULL
+getdata <- function()
+{return(x)}
+getinverse <- function() {inverse}
+setinverse <- function(i) 
+{
+  inverse<<-i
+  print("inverse has been stored")
+}
+list(getdata=getdata,getinverse=getinverse,setinverse=setinverse)
 
 }
 
 
-## Write a short comment describing this function
+## checks fro inverse value , if null , computes and stores the same.
+## if not reads the stored value.
+## After computation the function setinverse also displayes stating inverse
+## is being stored.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+if(!is.null(x$getinverse()))
+{
+  print("reading cached inverse")
+  d<-x$getinverse()
+  #inv<-x$getinverse()
+  return(d)
+}
+else
+{ olddata<-x$getdata()
+  print("Computing inverse")
+  inv<-solve(x$getdata())
+  x$setinverse(inv)
+  return(inv)
+}
 }
